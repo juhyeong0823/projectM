bb48467295a2f896074fac6c5f3fe1e448cf9973
starquf <starquf321@gmail.com>
2022-01-12 16:09:45 +0900

    웨이브 기능 구현

--- Assets/Scripts/Managers/MGWave.cs
+++ Assets/Scripts/Managers/MGWave.cs
@@ -6,6 +6,11 @@ public class MGWave : MonoBehaviour
 {
     public List<CONMonster> monsterList = new List<CONMonster>();
 
+    public int waveIdx = 1;
+
+    private float waveWaitTime = 2f;
+    private WaitForSeconds createWait = new WaitForSeconds(0.3f);
+
     private void Awake()
     {
         GameSceneClass.gMGWave = this;
@@ -13,14 +18,56 @@ public class MGWave : MonoBehaviour
 
     private void Start()
     {
-        CreateMonster();
-        CreateMonster();
-        CreateMonster();
+        StartCoroutine(WaveStart());
     }
 
     private void CreateMonster()
     {
         CONEntity monsterCon = GameSceneClass.gMGPool.CreateObj(ePrefabs.Monster, new Vector3(transform.position.x, Random.Range(-2f, 2f), 0f));
         GameSceneClass.gMGWave.monsterList.Add(monsterCon as CONMonster);
+    }
+
+    private IEnumerator WaveStart()
+    {
+        while (true)
+        {
+            float waveWait = 0f;
+
+            while (waveWait < waveWaitTime)
+            {
+                yield return null;
+
+                waveWait += Time.deltaTime;
+
+                GameSceneClass.gUIIngame.SetWaveInfo(waveIdx, waveWait, waveWaitTime);
+            }
+
+            float totalTime = waveIdx * 0.3f;
+            waveWait = totalTime;
+
+            StartCoroutine(CreateWaveMonster());
+
+            while (waveWait > 0f)
+            {
+                yield return null;
+
+                waveWait -= Time.deltaTime;
+
+                GameSceneClass.gUIIngame.SetWaveInfo(waveIdx, waveWait, totalTime);
+            }
+
+            yield return new WaitUntil(() => monsterList.Count == 0);
+
+            waveIdx++;
+        }
+    }
+
+    private IEnumerator CreateWaveMonster()
+    {
+        for (int i = 0; i < waveIdx; i++)
+        {
+            CreateMonster();
+            yield return createWait;
+        }
     }
 }
